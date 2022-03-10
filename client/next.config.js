a0b9f7d113a87f3c7334bdc3baae91c6393f4316
Andre Adikara <andrepith@yahoo.co.uk>
2022-03-10 14:52:05 +0700

    Use proxy api

--- client/next.config.js
+++ client/next.config.js
@@ -30,4 +30,12 @@ module.exports = {
       },
     ],
   ],
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:5000/api/:path*",
+      },
+    ];
+  },
 };
